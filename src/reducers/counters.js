45626d4e6a0cc11d4aa7eb3d646ed98867e76db6
wkda <stanislav.iakovlev@auto1.com>
2018-08-09 17:37:42 +0200

    add pointless counters

--- src/reducers/counters.js
+++ src/reducers/counters.js
@@ -1,7 +1,19 @@
+import { START_ALL_COUNTERS, STOP_ALL_COUNTERS } from '../actions/counters'
+
 const initalStore = {
   isRunning: true
 }
 
-export default function counters(state=initalStore, action={}) {
-  return state
+export default function counters(state = initalStore, action = {}) {
+  switch (action.type) {
+    case START_ALL_COUNTERS: {
+      return { isRunning: true }
+    }
+    case STOP_ALL_COUNTERS: {
+      return { isRunning: false }
+    }
+    default: {
+      return state
+    }
+  }
 }
