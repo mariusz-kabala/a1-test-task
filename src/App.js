f6e5f01c8e7e0354b6fb671694e341390a1e2dd9
Mariusz Kabała <mariusz.kabala@auto1.com>
2018-08-09 18:25:38 +0200

    Fix the header

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Auto1!</h1>
+          <h1 className="App-title">Welcome to AUTO1!</h1>
         </header>
         <div className="App-intro">
           <Row type="flex" justify="center">
